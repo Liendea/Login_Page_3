3916293680a7072e8ff35fc0d646d9f7d413face
Linda Bengtsson <linda-bengsson@outlook.com>
2025-01-22 22:46:16 +0100

    added if statement to enable proper class toggle on login.box

--- script.js
+++ script.js
@@ -9,13 +9,18 @@ var reset_btn = document.querySelector(".reset-submit");
 var reset_box = document.querySelector(".reset-box");
 var goBack_link = document.querySelector(".go-back");
 
-sign_in_btn.addEventListener("click", () => {});
-
 register_btn.addEventListener("click", () => {
   register_box.classList.toggle("hidden");
   register_start.classList.toggle("hidden");
   login_box.classList.toggle("hidden");
   login_start.classList.toggle("hidden");
+
+  if (!reset_box.classList.contains("hidden")) {
+    reset_box.classList.toggle("hidden");
+    if (!login_box.classList.contains("hidden")) {
+      login_box.classList.toggle("hidden");
+    }
+  }
 });
 
 sign_in_btn.addEventListener("click", () => {
@@ -30,12 +35,12 @@ reset_link.addEventListener("click", () => {
   login_box.classList.toggle("hidden");
 });
 
-reset_btn.addEventListener("clicked", () => {
+reset_btn.addEventListener("click", () => {
   login_box.classList.toggle("hidden");
   reset_box.classList.toggle("hidden");
 });
 
-goBack_link.addEventListener("clicked", () => {
+goBack_link.addEventListener("click", () => {
   reset_box.classList.toggle("hidden");
   login_box.classList.toggle("hidden");
 });
